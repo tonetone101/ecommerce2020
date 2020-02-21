becdef543231b630808f9037bf4908b9d949c7a9
Antkeo1 <sleav143@gmail.com>
2020-02-21 15:02:48 -0500

    creating user routes

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 require('dotenv').config()
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+// database
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}).then(() => console.log('DB connected'))
+
+// import routes
+const userRoutes = require('./routes/user')
+
+//routes
+app.use('/', userRoutes)
 
 const port = process.env.PORT || 8000
 
-app.listen(port)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
