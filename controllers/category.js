fc14763cbf49eefda2f6b248f7bc03b1ffc12076
Antkeo1 <sleav143@gmail.com>
2020-03-28 11:34:49 -0400

    CRUD operation for category is complete

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,19 @@
 const Category = require('../models/category')
 const {errorHandler} = require('../helpers/dbErrorHandlers')
+const _ = require('lodash')
+
+//middleware
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if(err || !category) {
+            return res.status(400).json({
+                error: "Category does not exist"
+            }) 
+        }
+        req.category = category
+        next()
+    })
+}
 
 exports.create = (req, res) => {
     const category = new Category(req.body)
@@ -13,3 +27,52 @@ exports.create = (req, res) => {
         res.json({data})
     })
 }
+
+exports.read = (req, res) => {
+    return res.json(req.category)
+}
+
+exports.update = (req, res) => {
+       //this is where the update happens
+       const category = req.category
+       category.name = req.body.name
+       //save the changes to DB
+       category.save((err, result) => {
+            if(err) {
+                console.log(err)
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result)
+       })
+}
+
+exports.remove = (req, res) => {
+    let category = req.category
+    category.remove((err, result) => {
+        if(err) {
+            console.log(err)
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        res.json({
+            message: "delete successful"
+        })
+    })
+}
+
+exports.list = (req, res) => {
+    Category.find().exec((err, data) => {
+        if(err) {
+            console.log(err)
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        res.json(data)
+    })
+}
+
+
