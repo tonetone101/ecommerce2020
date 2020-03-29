2f1b6635410a068c7686b5367b837b3090bacaa1
Antkeo1 <sleav143@gmail.com>
2020-03-29 12:18:49 -0400

    cors is install, time for React frontEnd

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const expressValidator = require('express-validator')
+
 
 require('dotenv').config()
 
@@ -20,6 +22,7 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(expressValidator())
+app.use(cors())
 
 // import routes
 const authRoutes = require('./routes/auth')
