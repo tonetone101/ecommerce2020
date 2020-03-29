5a778eb8d1ccf238f43e05266d3e5b20fed3e9d1
Antkeo1 <sleav143@gmail.com>
2020-03-29 12:04:17 -0400

    can now upload and view product images, Backend is complete!

--- models/product.js
+++ models/product.js
@@ -28,6 +28,10 @@ const productSchema = new mongoose.Schema(
     quanity: {
         type: Number
     },
+    sold: {
+        type: Number,
+        default: 0
+    },
     photo: {
         data: Buffer,
         contentType: String
--- routes/product.js
+++ routes/product.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
-const {create, productById, read, remove, update} = require('../controllers/product') 
+const {create, productById, photo, listBySearch, listCategories, read, remove, update, list, listRelated} = require('../controllers/product') 
 const {requireSignin, isAdmin, isAuth} = require('../controllers/auth') 
 const {userById} = require('../controllers/user') 
 
 router.get('/product/:productId', read)
+router.get('/products', list)
+router.get('/products/related/:productId', listRelated)
+router.get('/products/categories', listCategories)
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create)
+router.post("/products/by/search", listBySearch);
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove )
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update )
+
+// middleware to fetch photo
+router.get('/product/photo/:productId', photo)
 
 router.param('productId', productById)
 router.param('userId', userById)
