165cd57221e1aa000e06e5017a272cef45f58388
Antkeo1 <sleav143@gmail.com>
2020-05-15 11:56:44 -0400

    able to genreate token with braintree

--- app.js
+++ app.js
@@ -1,43 +1,46 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const expressValidator = require('express-validator')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
 
-
-require('dotenv').config()
+require("dotenv").config();
 
 // database
-mongoose.connect(process.env.MONGO_URI, {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true 
-}).then(() => console.log('DB connected'))
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"));
 
 //middlewares
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(expressValidator())
-app.use(cors())
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
 
 // import routes
-const authRoutes = require('./routes/auth')
-const userRoutes = require('./routes/user')
-const categoryRoutes = require('./routes/category')
-const productRoutes = require('./routes/product')
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const braintreeRoutes = require("./routes/braintree");
 
 //routes
-app.use('/', authRoutes)
-app.use('/', userRoutes)
-app.use('/', categoryRoutes)
-app.use('/', productRoutes)
+app.use("/", authRoutes);
+app.use("/", userRoutes);
+app.use("/", categoryRoutes);
+app.use("/", productRoutes);
+app.use("/", braintreeRoutes);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
