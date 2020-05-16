40f0564301b95570e62bea0dd5d30af052d32e31
Antkeo1 <sleav143@gmail.com>
2020-05-16 19:04:39 -0400

    can fetch user of each order

--- routes/order.js
+++ routes/order.js
@@ -2,7 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const { create, listOrder } = require("../controllers/order");
+const {
+  create,
+  listOrder,
+  getStatusValues,
+  orderById,
+  updateOrderStatus,
+} = require("../controllers/order");
 const { decreaseQuanity } = require("../controllers/product");
 
 router.post(
@@ -16,6 +22,22 @@ router.post(
 
 router.get("/order/list/:userId", requireSignin, isAuth, isAdmin, listOrder);
 
+router.get(
+  "/order/status-values/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  getStatusValues
+);
+router.put(
+  "/order/:orderId/status/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  updateOrderStatus
+);
+
 router.param("userId", userById);
+router.param("orderId", orderById);
 
 module.exports = router;
