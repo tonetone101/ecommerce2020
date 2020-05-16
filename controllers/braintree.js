afdffc9a75437e3ebf798f338361e08b92fd3aaa
Antkeo1 <sleav143@gmail.com>
2020-05-16 10:56:18 -0400

    order is saved into database

--- controllers/braintree.js
+++ controllers/braintree.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const gateway = braintree.connect({
   environment: braintree.Environment.Sandbox,
   //for production
-  // environment: braintree.Environment.Production,\
+  // environment: braintree.Environment.Production,
   merchantId: process.env.BRAINTREE_MERCHANT_ID,
   publicKey: process.env.BRAINTREE_PUBLIC_KEY,
   privateKey: process.env.BRAINTREE_PRIVATE_KEY,
