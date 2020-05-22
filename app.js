9d08206c5355bf3283e0545a7c37300d75ff1b2e
Antkeo1 <sleav143@gmail.com>
2020-05-22 09:42:34 -0400

    pushing to heroku

--- app.js
+++ app.js
@@ -41,6 +41,15 @@ app.use("/", productRoutes);
 app.use("/", braintreeRoutes);
 app.use("/", orderRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
